7b8a8550573c054997d1a5b5dee4912c991a5a55
Peyton Tanzillo <peyton.tanzillo@huskers.unl.edu>
2019-11-22 13:33:11 -0600

    Refactored box and row to be functional

--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -3,19 +3,18 @@ import './Row.css';
 
 import Square from '../Square/Square'
 
-class Row extends React.Component {
-  render() {
-    const { squareCount } = this.props
-    const squares = [];
-    for (let i = 0; i < squareCount; i++) {
-      squares.push(<Square key={i}/>)
-    }
-    return (
-      <div className="row">
-        {squares}
-      </div>
-    );
+function Row({
+  squareCount
+}) {
+  const squares = [];
+  for (let i = 0; i < squareCount; i++) {
+    squares.push(<Square key={i}/>)
   }
+  return (
+    <div className="row">
+      {squares}
+    </div>
+  );
 }
 
 export default Row;
